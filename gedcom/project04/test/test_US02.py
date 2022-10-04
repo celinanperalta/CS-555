84f1667d726e473dce2003bc2521b842af6d4cf5
serenadia <serena3106@gmail.com>
2022-10-03 23:43:01 -0400

    Changed validator.py

--- gedcom/project04/test/test_US02.py
+++ gedcom/project04/test/test_US02.py
@@ -8,14 +8,14 @@ from model import Individual, Family
 from validator import check_US02
 
 def test_birth_before_marriage(capfd):
-    obj = Family(id="F01", husband="", wife="", birth = datetime.datetime.now() + datetime.timedelta(days=1), marriage_date=datetime.datetime.now())
+    obj = Family(id="F01", husband="Sam", wife="Judy", birth = datetime.datetime.now() + datetime.timedelta(days=1), marriage_date=datetime.datetime.now())
     check_US02(obj)
     out, err = capfd.readouterr()
     assert out.strip() == consts.MSG_US02.format("Birth", obj.birth, obj.marriage_date)
 
 
 def test_valid_marriage(capfd):
-    obj = Family(id="FO1", husband="", wife="", marriage_date=datetime.datetime.now(), birth = datetime.datetime.now() + datetime.timedelta(days=1))
+    obj = Family(id="FO1", husband="Herbert", wife="Sherbet", marriage_date=datetime.datetime.now(), birth = datetime.datetime.now() + datetime.timedelta(days=1))
     check_US02(obj)
     out, err = capfd.readouterr()
     assert out.strip() == ""
@@ -27,7 +27,7 @@ def test_birth_no_marriage(capfd):
     assert out.strip() == ""
 
 def test_marriage_no_birth(capfd):
-    obj = Family(id="F01", husband="", wife="", marriage_date=datetime.datetime.now())
+    obj = Family(id="F01", husband="Max", wife="Ruby", marriage_date=datetime.datetime.now())
     check_US02(obj)
     out, err = capfd.readouterr()
     assert out.strip() == consts.MSG_US02.format(obj.wife, obj.husband, obj.birth, obj.marriage_date)
