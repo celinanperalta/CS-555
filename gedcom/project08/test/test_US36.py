d7e121c22fe1701ca50bbc89f6a1ef62e85ab828
serenadia <serena3106@gmail.com>
2022-11-01 00:24:50 -0400

    Fixed US 35, 36

--- gedcom/project08/test/test_US36.py
+++ gedcom/project08/test/test_US36.py
@@ -7,8 +7,8 @@ from src.validator import check_US36
 from util import gedcom_date_to_datetime
 
 def test_US36(capfd):
-    person1 = Individual("I01", "Kristen Smiles", "F", death=gedcom_date_to_datetime("12 OCT 2022"))
-    person2 = Individual("I02", "Jacke Paul", "M", death=gedcom_date_to_datetime("12 OCT 2002"))
+    person1 = Individual("Kristen Smiles", "F", death=gedcom_date_to_datetime("12 OCT 2022"))
+    person2 = Individual("Jacke Paul", "M", death=gedcom_date_to_datetime("12 OCT 2002"))
 
     individuals = [person1, person2]
 
